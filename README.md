8c8de8728035138a89b76b31a37c30e6a2568f9d
Seonwoo Kim <88951594+seki5405@users.noreply.github.com>
2023-07-02 10:46:25 -0600

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# DQfD - Deep Q-learning from Demonstrations
+
+## What is this?
+
+This repository contains the code for the paper [DQfD: Deep Q-learning from Demonstrations](https://arxiv.org/abs/1704.03732) by DeepMind.
---
+++ setup.py
@@ -0,0 +1,9 @@
+import io
+from setuptools import find_packages, setup
+
+setup(
+    name='DQfD-torch',
+    version='1.0.0',
+    description='DQfD torch implementation',
+    install_requires=['torch', 'numpy', 'matplotlib']
+)
